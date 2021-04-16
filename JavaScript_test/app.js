08e44326b009380b0d2375bae963e1b2bd789713
hikaru-webcamp <skyvader0524@gmail.com>
2021-04-16 18:00:47 +0900

    first commit

---
+++ JavaScript_test/app.js
@@ -0,0 +1,27 @@
+alert("Helllo World");
+
+var hello = "Hello World";
+alert(hello);
+var int1 = 1;
+
+var int2 = -40;
+
+var float1 = 3.14;
+
+var str1 = "JavaScriptを覚えよう";
+
+alert(4 + 3);
+alert(8 - 5);
+alert(2 * 6);
+alert(10 / 2);
+alert("Hello" + "World");
+
+var orange = 100;
+var apple = 120;
+if (orange < apple) {
+  alert("みかんの値段がりんごより安い");
+} else if (orange == apple) {
+  alert("みかんとりんごが同じ値段");
+} else {
+  alert("みかんの値段がりんごより高い");
+}
